efbf7bd59fc730c2936203e8ce8d0c17c1fb7314
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2025-10-25 23:46:56 -0500

    up to removeAt

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -4,6 +4,9 @@ class LinkedList {
   constructor() {
     this.#head = null;
   }
+  getHead() {
+    return this.#head;
+  }
   append(value) {
     const newNode = new Node(value, null);
 
@@ -13,10 +16,10 @@ class LinkedList {
     }
 
     let temp = this.#head;
-    while (temp.next !== null) {
-      temp = temp.next;
+    while (temp.nextNode !== null) {
+      temp = temp.nextNode;
     }
-    temp.next = newNode;
+    temp.nextNode = newNode;
   }
   prepend(value) {
     this.#head = new Node(value, this.#head);
@@ -28,7 +31,7 @@ class LinkedList {
 
     while (temp !== null) {
       size++;
-      temp = temp.next;
+      temp = temp.nextNode;
     }
 
     return size;
@@ -37,9 +40,10 @@ class LinkedList {
     return this.#head;
   }
   tail() {
+    if (!this.#head) return null;
     let temp = this.#head;
-    while (temp.next !== null) {
-      temp = temp.next;
+    while (temp.nextNode !== null) {
+      temp = temp.nextNode;
     }
     return temp;
   }
@@ -48,7 +52,7 @@ class LinkedList {
     let temp = this.#head;
     for (let i = 0; i < index; i++) {
       if (temp === null) return null;
-      temp = temp.next;
+      temp = temp.nextNode;
     }
     return temp;
   }
@@ -56,19 +60,76 @@ class LinkedList {
   pop() {
     if (this.#head === null) return null;
 
-    if (this.#head.next === null) {
+    if (this.#head.nextNode === null) {
       const popped = this.#head;
       this.#head = null;
       return popped;
     }
 
     let temp = this.#head;
-    while (temp.next.next !== null) {
-      temp = temp.next;
+    while (temp.nextNode.nextNode !== null) {
+      temp = temp.nextNode;
     }
 
-    const popped = temp.next;
-    temp.next = null;
+    const popped = temp.nextNode;
+    temp.nextNode = null;
     return popped;
   }
+  contains(value) {
+    if (!this.#head) return false;
+    let temp = this.#head;
+    while (temp !== null) {
+      if (temp.value === value) return true;
+      temp = temp.nextNode;
+    }
+    return false;
+  }
+  find(value) {
+    let temp = this.#head;
+    let index = 0;
+
+    while (temp !== null) {
+      if (temp.value === value) return index;
+      temp = temp.nextNode;
+      index++;
+    }
+
+    return null;
+  }
+  toString() {
+    let strList = "";
+    let temp = this.#head;
+    while (temp !== null) {
+      strList += `( ${temp.value} ) -> `;
+      temp = temp.nextNode;
+    }
+    strList += "null";
+    return strList;
+  }
+  insertAt(value, index) {
+    if (index < 0) return;
+    const newNode = new Node(value);
+
+    if (index === 0) {
+      newNode.nextNode = this.#head;
+      this.#head = newNode;
+      return;
+    }
+    const prevNode = this.at(index - 1);
+    if (prevNode === null) return;
+    newNode.nextNode = prevNode.nextNode;
+    prevNode.nextNode = newNode;
+  }
+  removeAt(index) {
+    if (index < 0 || this.#head === null) return false;
+    if (index === 0) {
+      this.#head = this.#head.nextNode;
+      return true;
+    }
+    const prevNode = this.at(index - 1);
+    if (prevNode === null || prevNode.nextNode === null) return false;
+    prevNode.nextNode = prevNode.nextNode.nextNode;
+    return true;
+  }
 }
+export default LinkedList;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import "./css/main.css";
-console.log("working");
+import LinkedList from "./LinkedList.js";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.getHead(), list.toString());
